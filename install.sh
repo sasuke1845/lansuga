2b5fe98054366768501a98ef596795c59f6e9600
sasuke1845 <agianakjb@gmail.com>
2024-08-29 21:08:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ display_welcome() {
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${BLUE}[+]                AUTO INSTALLER THEMA             [+]${NC}"
-  echo -e "${BLUE}[+]                  © LANSUGA OFFICIAL                [+]${NC}"
+  echo -e "${BLUE}[+]                  © LANSUGA OFFICIAL             [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${RED} [+] =============================================== [+]${NC}"
   echo -e ""
